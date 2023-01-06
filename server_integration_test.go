60fb912a2bef560fc4b98996eec4b58239e61880
fourth <fourthe.hero@gmail.com>
2023-01-06 22:40:42 +0700

    create server integation test

---
+++ server_integration_test.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreatedExpenser(t *testing.T) {
+	body := bytes.NewBufferString(`{
+		"title": "strawberry smoothie",
+		"amount": 82,
+		"note": "night market promotion discount 20 bath", 
+		"tags": ["food", "beverage"]
+	}`)
+	var e Expense
+
+	res := request(http.MethodPost, uri("expenses"), body)
+	err := res.Decode(&e)
+
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusCreated, res.StatusCode)
+	assert.NotEqual(t, 0, e.ID)
+	assert.Equal(t, "strawberry smoothie", e.Title)
+	assert.Equal(t, 82, e.Amount)
+	assert.Equal(t, "night market promotion discount 20 bath", e.Note)
+	assert.Equal(t, []string([]string{"food", "beverage"}), e.Tags)
+}
+
+type Response struct {
+	*http.Response
+	err error
+}
+
+func (r *Response) Decode(v interface{}) error {
+	if r.err != nil {
+		return r.err
+	}
+
+	return json.NewDecoder(r.Body).Decode(v)
+}
+func uri(paths ...string) string {
+	host := "http://localhost:2565"
+	if paths == nil {
+		return host
+	}
+
+	url := append([]string{host}, paths...)
+	return strings.Join(url, "/")
+}
+
+func request(method, url string, body io.Reader) *Response {
+	req, _ := http.NewRequest(method, url, body)
+	req.Header.Add("Content-Type", "application/json")
+	client := http.Client{}
+	res, err := client.Do(req)
+	return &Response{res, err}
+}
