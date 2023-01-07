f55de73f2a3a60f741b037692495bfff262b106d
fourth <fourthe.hero@gmail.com>
2023-01-07 22:58:04 +0700

    edit test create expense

--- handler/server_unit_test.go
+++ handler/server_unit_test.go
@@ -4,31 +4,31 @@
 package handler
 
 import (
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
-
-	"github.com/DATA-DOG/go-sqlmock"
-	"github.com/labstack/echo/v4"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestCreateExpense(t *testing.T) {
 	// Setup
 	e := echo.New()
 	expenseJSON := `{
+		"id": "1",
 		"title": "strawberry smoothie",
-		"amount": 82,
-		"note":"night market promotion discount 20 bath",
-		"tags":["food", "beverage"]
+		"amount": "82",
+		"note": "night market promotion discount 20 bath",
+		"tags": ["food", "beverage"]
 	}`
 	req := httptest.NewRequest(http.MethodPost, "/expenses", strings.NewReader(expenseJSON))
 	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 	rec := httptest.NewRecorder()
 
 	newsMockRows := sqlmock.NewRows([]string{"id", "title", "amount", "note", "tags"}).
-		AddRow(1, "strawberry smoothie", 82, "night market promotion discount 20 bath", `["food", "beverage"]`)
+		AddRow("1", "strawberry smoothie", "82", "night market promotion discount 20 bath", `["food", "beverage"]`)
 	db, mock, err := sqlmock.New()
 	mock.ExpectQuery("SELECT (.+) FROM expenses").WillReturnRows(newsMockRows)
 	if err != nil {
@@ -40,6 +40,6 @@ func TestCreateExpense(t *testing.T) {
 	// Assertions
 	if assert.NoError(t, h.CreatedExpenseHandler(c)) {
 		assert.Equal(t, http.StatusCreated, rec.Code)
-		assert.Equal(t, expenseJSON, rec.Body.String())
+		assert.Equal(t, expenseJSON, strings.TrimSpace(rec.Body.String()))
 	}
 }
